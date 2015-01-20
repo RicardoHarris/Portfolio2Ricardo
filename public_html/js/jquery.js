2776471086c74d4e144d2d613c63de47adfcc846
Ricardo Harris <ricardoharris@ymail.com>
2015-01-20 09:20:50 -0800

    New commit, Updating for jQuery

---
+++ public_html/js/jquery.js
@@ -0,0 +1,9 @@
+$("document").ready(function() {
+
+    $('#accord').accordion({header: "h2"});
+/*This line of code creates an accordian which activates when the header of an h2 is clicked.*/
+
+
+
+});
+
